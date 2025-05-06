c9a27022513c3636378e13ba8a3d8b9685ebdd03
Jack <jamaldonado@csumb.edu>
2025-05-06 13:01:21 -0700

    Added classes. Finished Item.java and Money.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,25 @@
 public class Item {
+    public String name;
+    public Money price;
+
+    public Item(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
 }
--- src/Money.java
+++ src/Money.java
@@ -1,2 +1,28 @@
 public class Money {
+    public static final Money ONE_DOLLAR = new Money(1.0);
+    private final long amount;
+
+    public Money(double amount) {
+        this.amount =Math.round(amount * 100);
+    }
+
+    public String getAmountString() {
+        return String.format("%.2f", getAmount());
+    }
+
+    public double getAmount() {
+        return this.amount / 100.0;
+    }
+
+    public Money add(Money add) {
+        return new Money(this.getAmount() + add.getAmount());
+    }
+
+    public Money subtract(Money subtract) {
+        return new Money(this.getAmount() - subtract.getAmount());
+    }
+
+    public String toString() {
+        return "$" + getAmountString();
+    }
 }
