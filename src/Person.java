e3fc8b48a6b2c6b01ac071e58bd55857dc248a5b
Jack <jamaldonado@csumb.edu>
2025-05-06 12:51:11 -0700

    Added classes. Finished Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Person {
+    public double totalDebt;
+    public ArrayList<Money> charge = new ArrayList<>();
+
+    public Person(double totalDebt) {
+        this.totalDebt = totalDebt;
+    }
+
+    public void addCharge(Money charge) {
+        this.charge.add(charge);
+    }
+
+    public void removeCharge(Money charge) {
+        this.charge.remove(charge);
+    }
+
+    public double getTotalDebt() {
+        return totalDebt;
+    }
+
+    public void setTotalDebt(double totalDebt) {
+        this.totalDebt = totalDebt;
+    }
+
+    public ArrayList<Money> getCharge() {
+        return charge;
+    }
+
+    public void setCharge(ArrayList<Money> charge) {
+        this.charge = charge;
+    }
 }
